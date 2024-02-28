12317e9427eb7d184366ae88af7447057ed8244a
TejasAnand <anandtejas455@gmail.com>
2024-02-28 17:06:31 -0500

    Add: tables for database

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,5 +1,27 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { drizzle } from "drizzle-orm/libsql";
 
 export const users = sqliteTable("users", {
-  id: text("id").primaryKey(),
+  id: text("userId").primaryKey(),
+});
+
+export const signInAccounts = sqliteTable("sign_in_accounts", {
+  googleUserId: text("googleId").primaryKey(),
+  id: text("userId"),
+});
+
+export const googleCalendar = sqliteTable("google_calendar", {
+  calendarId: text("calendarId").primaryKey(),
+  googleAccountId: text("userId"),
+  watchChannelId: text("watchChannelId"),
+  watchResourceId: text("watchResourceId"),
+  watchExpiration: integer("watchExpiration"),
+});
+
+export const googleCalendarAccounts = sqliteTable("google_calendar_accounts", {
+  googleAccountId: text("googleAccountId").primaryKey(),
+  userId: text("userId"),
+  accessToken: text("accessToken"),
+  refreshToken: text("refreshToken"),
+  accessTokenExpiration: integer("accessTokenExpiration"),
 });
